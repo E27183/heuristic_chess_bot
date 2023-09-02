3e6e549bcbfd31df41bd83e09112f4ca9e155dcf
Edan <edan.landow@gmail.com>
2023-09-02 13:54:42 +1000

    promotion

--- main.cpp
+++ main.cpp
@@ -8,11 +8,16 @@
 #include <FL/Fl_PNG_Image.H>
 #include <FL/Fl_Image.H>
 #include <FL/Fl_Button.H>
+#include <chrono>
 
 Fl_Button *boxes[8][8];
+Fl_Button *promotion_options[4];
 int indices[8][8];
+int promotion_indices[4] = {0, 1, 2, 3};
+
 move move_;
 bool first_move = true;
+bool waiting_for_promotion_decision = false;
 board board_;
 check_and_pin_feedback user_check_pin_feedback;
 
@@ -76,14 +81,30 @@ void callbackfunc(Fl_Widget * widget, void * data) {
         move_.square_from[1] = j;
         widget->color(fl_rgb_color(255, 0, 0));
     } else {
-        first_move = true;
-        move_.square_to[0] = i;
-        move_.square_to[1] = j;
-        is_checked_and_identify_pins_and_freedoms(&board_, board_.white_to_move, &user_check_pin_feedback);
-        if (valid_move(&move_, &board_, &user_check_pin_feedback)) {
-            make_move(&move_, &board_);
-            print_pieces_to_screen();
+        if (!waiting_for_promotion_decision) {
+            move_.square_to[0] = i;
+            move_.square_to[1] = j;
+            is_checked_and_identify_pins_and_freedoms(&board_, board_.white_to_move, &user_check_pin_feedback);
+            if (valid_move(&move_, &board_, &user_check_pin_feedback)) {
+                if ((board_.white_to_move && board_.piece_positions[move_.square_from[0]][move_.square_from[1]] == white_pawn && move_.square_to[1] == white_promote_row) ||
+                 (!board_.white_to_move && board_.piece_positions[move_.square_from[0]][move_.square_from[1]] == black_pawn && move_.square_to[1] == black_promote_row)) {
+                    waiting_for_promotion_decision = true;
+                    for (int i = 0; i < 4; i++) {
+                        promotion_options[i]->show();
+                    };
+                    return;
+                 } else {
+                    make_move(&move_, &board_);
+                    print_pieces_to_screen();
+                };
+            };
+        } else {
+            waiting_for_promotion_decision = false;
+            for (int i = 0; i < 4; i++) {
+                promotion_options[i]->box(FL_NO_BOX);
+            };
         };
+        first_move = true;
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if ((i + j) % 2 == 0) {
@@ -98,7 +119,59 @@ void callbackfunc(Fl_Widget * widget, void * data) {
     };
 };
 
+void promote_callbackfunc(Fl_Widget * widget, void * data) {
+    int index = *(int*)data;
+    first_move = true;
+    if (board_.white_to_move) {
+        switch (index) {
+            case 0:
+                make_move(&move_, &board_, white_queen);
+                break;
+            case 1:
+                make_move(&move_, &board_, white_rook);
+                break;
+            case 2:
+                make_move(&move_, &board_, white_bishop);
+                break;
+            case 3:
+                make_move(&move_, &board_, white_knight);
+                break;
+        };
+    } else {
+        switch (index) {
+            case 0:
+                make_move(&move_, &board_, black_queen);
+                break;
+            case 1:
+                make_move(&move_, &board_, black_rook);
+                break;
+            case 2:
+                make_move(&move_, &board_, black_bishop);
+                break;
+            case 3:
+                make_move(&move_, &board_, black_knight);
+                break;
+        };
+    };
+    print_pieces_to_screen();
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if ((i + j) % 2 == 0) {
+                boxes[i][j]->color(fl_rgb_color(100, 100, 100));
+                boxes[i][j]->redraw();
+            } else {
+                boxes[i][j]->color(fl_rgb_color(0, 0, 200));
+                boxes[i][j]->redraw();
+            };
+        };
+    };
+    for (int i = 0; i < 4; i++) {
+        promotion_options[i]->hide();
+    };
+};
+
 int main(int argc, char *argv[]) {
+    bool player_is_white = std::chrono::system_clock::now().time_since_epoch().count() % 2 == 0;
     int x;
     int y;
     int w;
@@ -136,7 +209,11 @@ int main(int argc, char *argv[]) {
     init_board(&board_);
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            boxes[i][j] = new Fl_Button(w_disp + (i * s) / 8, h_disp + (j * s) / 8 + 30, s / 8, s / 8);
+            if (player_is_white) {
+                boxes[i][j] = new Fl_Button(w_disp + (i * s) / 8, h_disp + ((7 - j) * s) / 8 + 30, s / 8, s / 8);
+            } else {
+                boxes[i][j] = new Fl_Button(w_disp + (i * s) / 8, h_disp + (j * s) / 8 + 30, s / 8, s / 8);
+            };
             boxes[i][j]->box(FL_UP_BOX);
             indices[i][j] = (i << 4) + j;
             boxes[i][j]->callback(callbackfunc, &indices[i][j]);
@@ -147,66 +224,31 @@ int main(int argc, char *argv[]) {
             };
         };
     };
+    for (int i = 0; i < 4; i++) {
+        promotion_options[i] = new Fl_Button(w_disp + (i * s) / 4, h_disp + 30, s / 4, s / 4);
+        promotion_options[i]->box(FL_UP_BOX);
+        if (player_is_white) {
+            promotion_options[i]->color(fl_rgb_color(0, 0, 0));
+        } else {
+            promotion_options[i]->color(fl_rgb_color(255, 255, 255));
+        };
+        promotion_options[i]->hide();
+        promotion_options[i]->callback(promote_callbackfunc, &promotion_indices[i]);
+    };
+    if (player_is_white) {
+        promotion_options[0]->image(images.white_queen);
+        promotion_options[1]->image(images.white_rook);
+        promotion_options[2]->image(images.white_bishop);
+        promotion_options[3]->image(images.white_knight);
+    } else {
+        promotion_options[0]->image(images.black_queen);
+        promotion_options[1]->image(images.black_rook);
+        promotion_options[2]->image(images.black_bishop);
+        promotion_options[3]->image(images.black_knight);
+    };
     print_pieces_to_screen();
 
     window->end();
     window->show(argc, argv);
     return Fl::run();
 };
-
-
- 
-// This defines the equivalent of main() for the current platform.
-
-// int main() {
-
-
-//     // board board_;
-//     // move output_move;
-//     // short promote_decision;
-//     // bool stale_or_checkmated;
-//     // init_board(&board_);
-//     // // search_for_best_move(&board_, &output_move, &promote_decision, 3, &stale_or_checkmated);
-//     // // std::cout << output_move.square_from[0] << "\n" << output_move.square_from[1] << "\n" << output_move.square_to[0] << "\n" << output_move.square_to[1] << "\n";
-//     // // // move m1 = {{4, 0}, {0, 6}};
-//     // // // move m2 = {{1, 1}, {1, 3}};
-//     // // // move m3 = {{0, 3}, {1, 2}};
-//     // // // make_move(&m1, &board_);
-//     // // // make_move(&m2, &board_);
-//     // // // make_move(&m3, &board_);
-//     // check_and_pin_feedback feedback;
-//     // // // reset_recent_move(&board_);
-//     // // // reset_recent_move(&board_);
-//     // // // reset_recent_move(&board_);
-//     // // // printf("%d", *std::begin(feedback.valid_squares));
-//     // // // printf("%s", feedback.valid_squares.count(7) ? "7" : "nah");
-//     // // // printf("%s", feedback.valid_squares.count(0) ? "0" : "nah");
-//     // // // print_board(&board_);
-//     // short x0;
-//     // short x1;
-//     // short x2;
-//     // short x3;
-//     // move move_;
-//     // print_board(&board_);
-//     // while (true) {
-//     //     is_checked_and_identify_pins_and_freedoms(&board_, board_.white_to_move, &feedback);
-//     //     std::cin >> x0;
-//     //     std::cin >> x1;
-//     //     std::cin >> x2;
-//     //     std::cin >> x3;
-//     //     move_.square_from[0] = x0;
-//     //     move_.square_to[0] = x2;
-//     //     move_.square_from[1] = x1;
-//     //     move_.square_to[1] = x3;
-//     //     if (valid_move(&move_, &board_, &feedback)) {
-//     //         make_move(&move_, &board_);
-//     //         print_board(&board_);
-//     //         search_for_best_move(&board_, &output_move, &promote_decision, 1, &stale_or_checkmated);
-//     //         make_move(&output_move, &board_, promote_decision);
-//     //         print_board(&board_);
-//     //     } else {
-//     //         printf("illegal\n");
-//     //     };
-//     // };
-//     return 0;
-// };
